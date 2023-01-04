81c43d32b317cb30a2d5b2da8e2a83239d70c80a
Zach <zachlandis91@gmail.com>
2023-01-04 10:47:22 -0700

    commit tree fcfa4c541b6d01de07b159124096da58a90189d3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -13,5 +15,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks(renderBooks);
 });
